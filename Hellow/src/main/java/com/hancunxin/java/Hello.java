4db31bd4fc8996a057ccb5bb72b61f9cd73fb11d
hancunxin <1003949051@qq.com>
2020-10-27 18:20:28 +0800

    添加文件

---
+++ Hellow/src/main/java/com/hancunxin/java/Hello.java
@@ -0,0 +1,14 @@
+package com.hancunxin.java;
+
+/**
+ * Description:
+ *
+ * @author HanCunxin
+ * @version 1.0
+ * @date:2020/10/27 18:18
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("fuck world");
+    }
+}
